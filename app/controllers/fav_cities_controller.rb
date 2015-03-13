49d03e925199455ed5ee98ccc63b5a0d0aa08c35
adrienfried <adrien.fried@free.fr>
2015-03-12 20:33:47 -0700

    added store, retrieve and delete for favorite cities

--- app/controllers/fav_cities_controller.rb
+++ app/controllers/fav_cities_controller.rb
@@ -1,5 +1,46 @@
 class FavCitiesController < ApplicationController
+    include FavCitiesHelper
+    include ApplicationHelper
+      
   def index
-    
+      
+      userId = "100184922779584864374"
+      cities = Database.retrieveAllFavoriteCities(userId)
+  end
+  
+  def new
+  
+  end
+  
+  def create
+      city = Hash.new
+      city[:name] = params[:city]["name"]
+      
+      city[:cityId] = OpenWeather.getCitiesIDs(params[:city]["name"],1)[0]
+      
+      latLon = Geocoder.getLatLon(params[:city]["name"])
+      
+      latitude = latLon[:lat]
+      longitude = latLon[:lng]
+
+      city[:latitude] = latitude
+      city[:longitude] = longitude
+      
+      userId = "100184922779584864374"
+      
+      Database.storeFavoriteCity(city, userId)
+      puts Database.retrieveAllFavoriteCities(userId)
+      redirect_to fav_cities_path
+  end
+  
+  def show
+      userId = "100184922779584864374"
+      cities = Database.retrieveAllFavoriteCities(userId)
+  end
+  
+  def delete
+      userId = "100184922779584864374"
+      
+      deleteFavoriteCity(userId, cityId)
   end
 end
--- app/helpers/fav_cities_helper.rb
+++ app/helpers/fav_cities_helper.rb
@@ -1,2 +1,41 @@
+require 'orchestrate'
+require 'httparty'
+require 'json'
+
 module FavCitiesHelper
+    
+    class Database
+        
+        ORC_API_KEY = "f72b43bb-175a-49ea-826e-dded02aa73f6";
+
+        def self.storeFavoriteCity (city, userId)
+            client = Orchestrate::Client.new(ORC_API_KEY);
+            cityId = city[:cityId]
+            puts city
+            client.put(:favoriteCities, cityId, city)
+            client.put_relation(:googleuser, userId, "favorite city", :favoriteCities, cityId)
+            puts "relations: ", client.get_relations(:googleuser, userId, :city)
+        end
+    
+        def self.retrieveAllFavoriteCities (userId)
+        
+            client = Orchestrate::Client.new(ORC_API_KEY);
+            
+            jsonOfFavCities = client.get_relations(:googleuser, userId, :city)
+            puts "this is jsonoffavcities", jsonOfFavCities
+            
+            #listOfFavCities = JSON.parse(jsonOfFavCities.to_s)
+            #puts "this is listoffavcities", listOfFavCities
+            
+            #return listOfFavCities
+        end
+
+        def self.deleteFavoriteCity(userId, cityId)
+
+            client = Orchestrate::Client.new(ORC_API_KEY);
+
+            client.delete(:favoriteCities, cityId)
+            client.delete_relation(:users, "kates-user-id", :follows, :users, "robs-user-id")
+        end
+    end
 end
