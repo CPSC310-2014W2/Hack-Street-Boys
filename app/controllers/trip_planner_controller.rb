3e1cea416cafa3bad9643346b9e94058ee56c619
adrienfried <adrien.fried@free.fr>
2015-03-28 23:43:41 -0700

    improved tirp planner

--- app/controllers/trip_planner_controller.rb
+++ app/controllers/trip_planner_controller.rb
@@ -1,24 +1,28 @@
+require 'date'
 class TripPlannerController < ApplicationController
-    include ApplicationHelper
+  include ApplicationHelper
     
-    def index
-        
-           if (params[:city] != nil)
-               
-               puts "this is forecastdata: ", @forecastData
-               name = params[:city]["name"]
-        
-                puts name
-            
-                @geoInfo = Geocoder.getGeoInfo(name)
-                @forecastData = OrchestrateDatabase.getCityWeatherData(@geoInfo)
+  def index
 
-           end
-        
+    if (params[:city] != nil)
+
+      @name = params[:city]["name"]
+      @geoInfo = Geocoder.getGeoInfo(@name)
+      @forecastData = OrchestrateDatabase.getCityWeatherData(@geoInfo)
+      @weekData = @forecastData["daily_this_week"]
+
+      @forecastArray = Array.new
+      @timeArray = Array.new
+      @summaryArray = Array.new
+      @iconArray = Array.new
+
+
+      @weekData["data"].each do |data|
+        @timeArray << Time.at(data["time"]).to_date.  strftime('%a %d %b %Y')
+        @summaryArray << data["summary"]
+        @iconArray << "<img src=\"/assets/#{data["icon"]}.png\" alt=\"some_text\" style=\"width:60px;height:60px\">"
+      end
+      puts @timeArray
     end
-    
-    def new
-        
-    end
-    
+  end
 end
