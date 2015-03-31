cb64a08c6d52454eaa8c65c8635ece89f1faf021
adrienfried <adrien.fried@free.fr>
2015-03-31 13:37:51 -0700

    added error checking

--- app/controllers/trip_planner_controller.rb
+++ app/controllers/trip_planner_controller.rb
@@ -9,18 +9,20 @@ class TripPlannerController < ApplicationController
       @name = params[:city]["name"]
       @geoInfo = Geocoder.getGeoInfo(@name)
       @forecastData = OrchestrateDatabase.getCityWeatherData(@geoInfo)
-      @weekData = @forecastData["daily_this_week"]
+      
+      if (@forecastData != nil)
+        @weekData = @forecastData["daily_this_week"]
 
-      @forecastArray = Array.new
-      @timeArray = Array.new
-      @summaryArray = Array.new
-      @iconArray = Array.new
+        @timeArray = Array.new
+        @summaryArray = Array.new
+        @iconArray = Array.new
 
 
-      @weekData["data"].each do |data|
-        @timeArray << Time.at(data["time"]).to_date.  strftime('%a %d %b %Y')
-        @summaryArray << data["summary"]
-        @iconArray << data["icon"]
+        @weekData["data"].each do |data|
+          @timeArray << Time.at(data["time"]).to_date.strftime('%a %d %b %Y')
+          @summaryArray << data["summary"]
+          @iconArray << data["icon"]
+        end
       end
     end
   end
